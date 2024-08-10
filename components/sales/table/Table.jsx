084463f58cb98454ec1a4582ff174f920720d14f
Kennyegun24 <kennyegun240@gmail.com>
2024-08-10 12:49:32 +0100

    Mod

--- components/sales/table/Table.jsx
+++ components/sales/table/Table.jsx
@@ -8,46 +8,53 @@ import { Avatar, Box } from "@mui/material";
 
 const generateColumn = () => {
   const prods = Object.keys(products[0]);
+  const excludedColumns = [
+    "shippingTime",
+    "productDescription",
+    "variants",
+    "supplierContact",
+  ];
 
-  const columns = prods.map((key) => {
-    let headerName = key
-      .replace(/([A-Z])/g, " $1")
-      .replace(/^./, (str) => str.toUpperCase());
+  const columns = prods
+    .filter((fil) => !excludedColumns.includes(fil))
+    .map((key) => {
+      let headerName = key
+        .replace(/([A-Z])/g, " $1")
+        .replace(/^./, (str) => str.toUpperCase());
 
-    if (key === "images") {
+      if (key === "images") {
+        return {
+          renderCell: (params) => {
+            return (
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <Avatar src={params?.row?.images[0]} />
+              </Box>
+            );
+          },
+          field: key,
+          headerName: headerName,
+          width: 150,
+          editable: true,
+          headerClassName: "super-app-theme--header super-app-image",
+          headerAlign: "center",
+        };
+      }
       return {
-        renderCell: (params) => {
-          console.log(params);
-          return (
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "100%",
-              }}
-            >
-              <Avatar src={params?.row?.images[0]} />
-            </Box>
-          );
-        },
         field: key,
         headerName: headerName,
         width: 150,
         editable: true,
-        headerClassName: "super-app-theme--header super-app-image",
-        headerAlign: "center",
+        headerClassName: "super-app-theme--header",
       };
-    }
-    return {
-      field: key,
-      headerName: headerName,
-      width: 150,
-      editable: true,
-      headerClassName: "super-app-theme--header",
-    };
-  });
+    });
 
   return columns;
 };
--- components/toolKits/TableToolBar.jsx
+++ components/toolKits/TableToolBar.jsx
@@ -1,5 +1,4 @@
 import {
-  DataGrid,
   GridToolbarContainer,
   gridPaginatedVisibleSortedGridRowIdsSelector,
   gridSortedRowIdsSelector,
@@ -8,6 +7,7 @@ import {
 } from "@mui/x-data-grid";
 import { Button } from "@mui/material";
 import { FaDownload } from "react-icons/fa6";
+import jsPDF from "jspdf";
 
 const getRowsFromCurrentPage = ({ apiRef }) =>
   gridPaginatedVisibleSortedGridRowIdsSelector(apiRef);
@@ -17,16 +17,50 @@ const getUnfilteredRows = ({ apiRef }) => gridSortedRowIdsSelector(apiRef);
 const getFilteredRows = ({ apiRef }) =>
   gridExpandedSortedRowIdsSelector(apiRef);
 
-const Toolbar = () => {
+const Toolbar = (props) => {
   const apiRef = useGridApiContext();
   const handleExport = (options) => apiRef.current.exportDataAsCsv(options);
+  const getRowData = (rowIds) => {
+    return rowIds.map((id) => apiRef.current.getRow(id));
+  };
+  const exportToPDF = () => {
+    // const rowIds = getFilteredRows({ apiRef });
+    // const tableRows = getRowData(rowIds);
+
+    // const tableColumn = columns.map((col) => col.headerName);
+    // const tableData = tableRows.map((row) =>
+    //   columns.map((col) => row[col.field])
+    // );
+
+    // const doc = new jsPDF();
+
+    // // Add title
+    // doc.setFontSize(18);
+    // doc.text("Executive Summary", 14, 22);
+
+    // // Add date
+    // const date = new Date();
+    // doc.setFontSize(11);
+    // doc.text(`Report generated on: ${date.toLocaleDateString()}`, 14, 30);
+
+    // // Add table
+    // doc.autoTable({
+    //   head: [tableColumn],
+    //   body: tableData,
+    //   startY: 40,
+    // });
+
+    // doc.save("executive-summary.pdf");
+    console.log(props);
+  };
+
   const buttonBaseProps = {
     color: "primary",
     size: "small",
     startIcon: <FaDownload />,
   };
   return (
-    <GridToolbarContainer>
+    <GridToolbarContainer {...props}>
       <Button
         {...buttonBaseProps}
         onClick={() =>
@@ -47,6 +81,9 @@ const Toolbar = () => {
       >
         Unfiltered rows
       </Button>
+      {/* <Button {...buttonBaseProps} onClick={exportToPDF}>
+        Export as PDF
+      </Button> */}
     </GridToolbarContainer>
   );
 };
