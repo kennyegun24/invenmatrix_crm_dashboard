818e9d0a0f61a6a50b16aac0ebda005e8cbd40a9
Kennyegun24 <kennyegun240@gmail.com>
2024-08-11 03:41:02 +0100

    Modify

--- components/grid/GridFolder.jsx
+++ components/grid/GridFolder.jsx
@@ -3,11 +3,31 @@ import "./gridfolder.css";
 import Image from "next/image";
 import { BsFolder } from "react-icons/bs";
 import { GoStack } from "react-icons/go";
+import { MdOutlineEdit } from "react-icons/md";
+import { LuFolderInput } from "react-icons/lu";
+import { FaEllipsisV } from "react-icons/fa";
 
 const GridFolder = ({ image }) => {
   return (
     <div className="flex column grid_folder_component pointer">
-      <Image src={image} />
+      <section className="grid_folder_image_div">
+        <Image src={image} />
+
+        <div className="absolute display_on_hover flex justify_between">
+          <input type="checkbox" name="" id="" />
+          <div className="flex column justify_between">
+            <section className="flex column gap05rem">
+              <div className="hover_icon">
+                <MdOutlineEdit />
+              </div>
+              <div className="hover_icon">
+                <LuFolderInput />
+              </div>
+            </section>
+            <FaEllipsisV />
+          </div>
+        </div>
+      </section>
       <div className="flex column gap05rem wrap grid_folder_content">
         <h4>Glasses</h4>
         <div className="flex gap05rem align_center">
--- components/grid/GridItem.jsx
+++ components/grid/GridItem.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./gridfolder.css";
 import Image from "next/image";
+import { MdOutlineEdit } from "react-icons/md";
+import { LuFolderInput } from "react-icons/lu";
+import { FaEllipsisV } from "react-icons/fa";
 
 const GridItem = ({ image }) => {
   return (
     <div className="flex column grid_folder_component pointer">
-      <Image src={image} />
+      <section className="grid_folder_image_div">
+        <Image src={image} />
+
+        <div className="absolute display_on_hover flex justify_between">
+          <input type="checkbox" name="" id="" />
+          <div className="flex column justify_between align_end">
+            <section className="flex column gap05rem align_end">
+              <div className="hover_icon">
+                <MdOutlineEdit />
+              </div>
+              <p className="hover_icon">Add to folder</p>
+            </section>
+            <FaEllipsisV />
+          </div>
+        </div>
+      </section>
       <div className="flex column gap3rem wrap grid_item_content">
         <h4>Glasses</h4>
         <div className="flex gap05rem align_center">
