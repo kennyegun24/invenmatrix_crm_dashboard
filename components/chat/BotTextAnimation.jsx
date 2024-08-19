18ea3d2d9b81d51b0033c0a81f77ff6a8c155fac
Kennyegun24 <kennyegun240@gmail.com>
2024-08-19 20:21:02 +0100

    Auto slider on bot

---
+++ components/chat/BotTextAnimation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./botTextAnimation.css";
+
+const BotTextAnimation = () => {
+  return <div className="dot-flashing " />;
+};
+
+export default BotTextAnimation;
--- components/chat/Chats.jsx
+++ components/chat/Chats.jsx
@@ -1,42 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./chat.css";
 import ChatBotHeader from "./ChatBotHeader";
 import { FaPaperPlane } from "react-icons/fa6";
+import BotTextAnimation from "./BotTextAnimation";
 
 const Chats = () => {
   const [messages, setMessages] = useState([]);
   const [userText, setUserText] = useState("");
+  const [generatingText, setGeneratingText] = useState(false);
+  const smoothSlide = useRef();
   const handleSend = async () => {
-    setMessages((prev) => [
-      ...prev,
-      {
-        message: userText,
-        id: "user",
-      },
-    ]);
-    const fetchData = await fetch("/api/bot", {
-      method: "POST",
-      body: JSON.stringify({
-        message: userText,
-      }),
-    });
-    const data = await fetchData.json();
-    console.log(data);
-    setMessages((prev) => [
-      ...prev,
-      {
-        message: data.message,
-        id: data.id,
-      },
-    ]);
-    setUserText("");
+    if (userText.trim !== "") {
+      setMessages((prev) => [
+        ...prev,
+        {
+          message: userText,
+          id: "user",
+        },
+      ]);
+      setGeneratingText(true);
+      const fetchData = await fetch("/api/bot", {
+        method: "POST",
+        body: JSON.stringify({
+          message: userText,
+        }),
+      });
+      const data = await fetchData.json();
+      if (data) {
+        setTimeout(() => {
+          setGeneratingText(false);
+          setMessages((prev) => [
+            ...prev,
+            {
+              message: data.message,
+              id: data.id,
+            },
+          ]);
+          setUserText("");
+        }, 300);
+      }
+    }
   };
+  useEffect(() => {
+    smoothSlide.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className="chat-bot-chat flex column justify_between">
       <ChatBotHeader />
       <div className="flex column gap1rem chat-texts-container">
         {messages.map((m, _) => (
           <div
+            ref={smoothSlide}
             key={_}
             className={`chat_text_div flex ${
               m.id === "user" && "chat_reverse"
@@ -45,6 +60,14 @@ const Chats = () => {
             <p className="chat-text">{m.message}</p>
           </div>
         ))}
+        {generatingText && (
+          <div
+            className={`chat_text_div flex chat-text justify_center`}
+            style={{ width: 70, padding: "0.5rem 0.25rem" }}
+          >
+            <BotTextAnimation />
+          </div>
+        )}
       </div>
 
       <div className="flex gap05rem width100 chat_bot_input_div pointer">
