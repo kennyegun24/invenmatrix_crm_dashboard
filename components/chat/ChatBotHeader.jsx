65145cc65d21564dc3a08d58f554f4e675e933c5
Kennyegun24 <kennyegun240@gmail.com>
2024-08-19 14:04:42 +0100

    Bot chat screen

---
+++ components/chat/ChatBotHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ChatBotHeader = () => {
+  return (
+    <div className="flex align_center gap05rem">
+      <div className="bot_header_img" />
+      <h3>MatriBot</h3>
+    </div>
+  );
+};
+
+export default ChatBotHeader;
--- components/chat/Chats.jsx
+++ components/chat/Chats.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import "./chat.css";
+import ChatBotHeader from "./ChatBotHeader";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chat-bot-chat flex column justify_between">
+      <ChatBotHeader />
+      <div className="flex column gap1rem chat-texts-container">
+        <div className="chat_text_div flex chat_reverse">
+          <p className="chat-text">
+            This is a message hjg g hjg hjg fhgvjftf kyytsresh
+          </p>
+        </div>
+        <div className="chat_text_div flex chat_reverse">
+          <p className="chat-text">This is a message</p>
+        </div>
+        <div className="chat_text_div flex">
+          <p className="chat-text">This is a message</p>
+        </div>
+        <div className="chat_text_div flex chat_reverse">
+          <p className="chat-text">This is a message</p>
+        </div>
+        <div className="chat_text_div flex">
+          <p className="chat-text">
+            This is a message hjg g hjg hjg fhgvjftf kyytsresh
+          </p>
+        </div>
+        <div className="chat_text_div flex">
+          <p className="chat-text">This is a message</p>
+        </div>
+        <div className="chat_text_div flex chat_reverse">
+          <p className="chat-text">This is a message</p>
+        </div>
+        <div className="chat_text_div flex">
+          <p className="chat-text">This is a message</p>
+        </div>
+        <div className="chat_text_div flex chat_reverse">
+          <p className="chat-text">
+            This is a message hjg g hjg hjg fhgvjftf kyytsresh
+          </p>
+        </div>
+        <div className="chat_text_div flex">
+          <p className="chat-text">This is a message</p>
+        </div>
+        <div className="chat_text_div chat_reverse flex">
+          <p className="chat-text">This is a message</p>
+        </div>
+        <div className="chat_text_div chat_reverse flex">
+          <p className="chat-text">This is a message</p>
+        </div>
+      </div>
+      <input type="text" placeholder="Type message here..." />
+    </div>
+  );
 };
 
 export default Chats;
